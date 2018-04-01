6c57351e3e16554d0431a5d7605c82fbd3bd8f5d
Martez Reed <martez.reed@slalom.com>
2018-03-31 22:52:04 -0500

    Fix cloudtrail syntax error

--- controls/cloudtrail_enabled.rb
+++ controls/cloudtrail_enabled.rb
@@ -2,7 +2,7 @@ control 'cloudtrail_enabled' do
   title 'CloudTrail must be enabled'
   impact 1.0
 
-  describe aws_cloudtrail_trails
+  describe aws_cloudtrail_trails do
     it { should exist }
   end   
 end
